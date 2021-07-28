2375a1d9110e77c97cc6b01d2a4a6eb0f6fded50
Xu-You Liu <kangarooonetwo@gmail.com>
2021-07-28 18:04:05 +0800

    update fmriprep anat directory

--- preprocessing/deface_template.sh
+++ preprocessing/deface_template.sh
@@ -1,16 +1,16 @@
 #! /bin/bash
 
 # LOAD GLOBAL VARIABLES AND MODULES ON THE CLUSTER
-source globals.sh   
-module load fsl/6.0.2
+source globals.sh
+module load fsl/6.0.4
 module load pydeface/2.0.0
 
 sid=$1
 
 subj_dir=sub-$sid
 
-T1=$bids_dir/derivatives/fmriprep/$subj_dir/anat/${subj_dir}_desc-preproc_T1w.nii.gz
+T1=$bids_dir/derivatives/fmriprep/$subj_dir/ses-01/anat/${subj_dir}_ses-01_desc-preproc_T1w.nii.gz
 pydeface $T1
 
-T1_defaced=$bids_dir/derivatives/fmriprep/$subj_dir/anat/${subj_dir}_desc-preproc_T1w_defaced.nii.gz
+T1_defaced=$bids_dir/derivatives/fmriprep/$subj_dir/ses-01/anat/${subj_dir}_ses-01_desc-preproc_T1w_defaced.nii.gz
 mv $T1_defaced $defaced_dir

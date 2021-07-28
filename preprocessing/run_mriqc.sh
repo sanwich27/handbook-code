45c65ec7098e9334cd68dcdb213d6c4579108901
Xu-You Liu <kangarooonetwo@gmail.com>
2021-07-28 18:04:41 +0800

    update MRIQC version; send work to scratch

--- preprocessing/run_mriqc.sh
+++ preprocessing/run_mriqc.sh
@@ -2,10 +2,18 @@
 
 source globals.sh
 
-singularity run --cleanenv \
-    --bind $bids_dir:/home \
-    /jukebox/hasson/singularity/mriqc/mriqc-v0.10.4.sqsh \
-    --participant-label sub-$1 \
-    --correct-slice-timing --no-sub \
-    --nprocs 8 -w /home/derivatives/work \
-    /home /home/derivatives/mriqc participant
+#singularity run --cleanenv \
+#	    --bind $bids_dir:/bids \
+#	        --bind $scratch_dir:/scratch \
+#		    --bind /usr/people \
+#		        /jukebox/hasson/singularity/mriqc/mriqc-v0.15.1.simg \
+#			    --participant-label $1 \
+#			        --correct-slice-timing --no-sub \
+#				    --nprocs 8 -w /scratch \
+#				        /bids /bids/derivatives/mriqc participant
+docker run -it --rm -v $bids_dir:/bids:ro -v $scratch_dir:/scratch \
+	poldracklab/mriqc:latest \
+	--participant-label $1 \
+	--correct-slice-timing --no-sub \
+	--nprocs 8 -w /scratch \
+	/bids /bids/derivatives/mriqc participant
--- preprocessing/run_mriqc_group.sh
+++ preprocessing/run_mriqc_group.sh
@@ -2,10 +2,16 @@
 
 source globals.sh
 
-singularity run --cleanenv \
-    --bind $bids_dir:/home \
-    /jukebox/hasson/singularity/mriqc/mriqc-v0.10.4.sqsh \
-    --correct-slice-timing --modalities T1w bold \
-    --no-sub \
-    --nprocs 8 -w /home/derivatives/work \
-    /home /home/derivatives/mriqc group
+#singularity run --cleanenv \
+#    --bind $bids_dir:/home \
+#    /jukebox/hasson/singularity/mriqc/mriqc-v0.10.4.sqsh \
+#    --correct-slice-timing --modalities T1w bold \
+#    --no-sub \
+#    --nprocs 8 -w /home/derivatives/work \
+#    /home /home/derivatives/mriqc group
+docker run -it --rm -v $bids_dir:/bids:ro -v $scratch_dir:/scratch \
+	poldracklab/mriqc:latest \
+	--modalities T1w bold  \
+	--correct-slice-timing --no-sub \
+	--nprocs 8 -w /scratch \
+	/bids /bids/derivatives/mriqc group
